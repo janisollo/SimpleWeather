d2f7e8d1e17f780f3c29f0b2da9840d8af810cf6
janisollo <sollo.jani@gmail.com>
2022-03-24 12:50:47 +0700

    Moving on

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ app.get("/", function (req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=2c5dcfbf7ebd20e55dc2279525d09593&units=metric"
 
     https.get(url, function (response) {
-        console.log(response)
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weartherDescription = weatherData.weather[0].description
+            res.write("<h1>The temperature in Bangkok is " + temp + " Degrees celcius. While the sky above us has " + weartherDescription + ".</h1>")
+
+            res.send()
+        })
     })
 
-    res.send("Server is up and running")
 })
 
 
